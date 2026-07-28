d092e35fb2603bb84010ea5872cd6aa29566b00d
hossein <hosseinfaos1386@gmail.com>
2026-07-28 19:39:06 +0330

    front home-02

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{{asset('assets/js/tailwind.js')}}"></script>
-    <script src="{{ asset('assets/js/jquery.js') }}"></script>
+    <script src="{{asset('assets/tailwind.js')}}"></script>
+    <script src="{{ asset('assets/jquery.js') }}"></script>
     <!-- <link rel="stylesheet" href="{{asset('assets/css/style.css')}}"> -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
     <title>Document</title>
