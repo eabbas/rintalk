4d64163fe6f5c93193dcf1f37d24327b3005c783
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-08-18 18:52:28 +0330

    debugged

--- resources/views/admin/course/listcourseuser.blade.php
+++ resources/views/admin/course/listcourseuser.blade.php
@@ -20,7 +20,7 @@
             right: 3px;
         }
     </style>
-    <div class="w-full bg-white pt-3">
+    <div class="w-full bg-white p-3">
         <div class="w-11/12 mx-auto flex flex-row justify-between mt-3">
             <div class="flex bg-white rounded-md box-shadow px-3 py-1">
                 <svg xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +47,7 @@
         </div>
         @foreach ($courses as $course)
             <a href="{{ route('course.single', $course->id) }}" class="w-11/12 mx-auto flex gap-3">
-                <div class="w-full bg-white box-shadow rounded-lg mt-3 flex flex-row-reverse items-center gap-2 relative px-1">
+                <div class="w-full bg-white box-shadow rounded-lg mt-3 flex flex-row-reverse items-center gap-2 relative p-2">
                     <div class="w-3/24 h-full flex items-center justify-center">
                         <div class="w-5 h-5 rounded-full flex items-center justify-center box-shadow">
                             <svg xmlns="http://www.w3.org/2000/svg" class="fill-[#fe5d07] size-3 rotate-y-180" viewBox="0 0 320 512">
@@ -64,24 +64,24 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-[#484a65] size-2" viewBox="0 0 512 512">
                                         <path d="M64 64C46.3 64 32 78.3 32 96V416c0 17.7 14.3 32 32 32H448c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H291.9c-17 0-33.3-6.7-45.3-18.7L210.7 73.4c-6-6-14.1-9.4-22.6-9.4H64zM0 96C0 60.7 28.7 32 64 32H188.1c17 0 33.3 6.7 45.3 18.7l35.9 35.9c6 6 14.1 9.4 22.6 9.4H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z"/>
                                     </svg>
-                                    <span>To-Do List</span>
+                                    <span>سطح {{ $course->level->title }}</span>
                                 </div>
                                 <div class="bg-gray-100 text-[9px] text-[#484a65] rounded-md flex gap-1 p-1 justify-center items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-[#484a65] size-2" viewBox="0 0 384 512">
                                         <path d="M352 448V192H240c-26.5 0-48-21.5-48-48V32H64C46.3 32 32 46.3 32 64V448c0 17.7 14.3 32 32 32H320c17.7 0 32-14.3 32-32zm-.5-288c-.7-2.8-2.1-5.4-4.2-7.4L231.4 36.7c-2.1-2.1-4.6-3.5-7.4-4.2V144c0 8.8 7.2 16 16 16H351.5zM0 64C0 28.7 28.7 0 64 0H220.1c12.7 0 24.9 5.1 33.9 14.1L369.9 129.9c9 9 14.1 21.2 14.1 33.9V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z"/>
                                     </svg>
-                                    <span class="in-fa">5موضوع</span>
+                                    <span class="in-fa">{{ $course->master_name }}</span>
                                 </div>
                                 <div class="bg-gray-100 text-[9px] text-[#484a65] rounded-md flex gap-1 p-1 justify-center items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-[#484a65] size-2" stroke-width="2" viewBox="0 0 512 512">
                                         <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
                                     </svg>
-                                    <span class="in-fa">5تا6ساعت</span>
+                                    <span class="in-fa">{{ $course->duration }} ساعت</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="min-w-15 max-w-15 h-15 flex items-center justify-center">
+                    <div class="min-w-14 max-w-14 h-14 flex items-center justify-center">
                         <img class="w-full h-full rounded-2xl object-cover" src="{{asset('storage/'.$course->image)}}" alt="">
                     </div>
                 </div>
